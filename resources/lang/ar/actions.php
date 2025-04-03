66132d9284dbdb3e113398ab637f24b683e84806
Zayed Adel Al-Meklahfi <zayedadel95@gmail.com>
2025-04-04 02:11:42 +0300

    Add files via upload

---
+++ resources/lang/ar/actions.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'previous_version' => 'السابق',
+    'next_version' => 'التالي',
+    'restore' => [
+        'label' => 'استعادة هذه النسخة',
+        'modal_description' => 'هل انت متاكد?',
+        'modal_submit_action_label' => 'نعم',
+    ],
+    'revisions' => 'التعديلات',
+];
---
+++ resources/lang/ar/page.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'title' => 'مقارنة تعديلات :title',
+    'breadcrumb' => 'تعديلات',
+    'content_tab_label' => 'التعديلات',
+    'revision_by' => 'عدل من قبل :name',
+    'revisions_list' => 'قائمة التعديلات',
+    'anonymous_user' => 'مستخدم مجهول',
+];
